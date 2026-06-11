48b4a7d2e584ff25314d10d5ce509493a594e90f
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 22:39:06 +0530

    Fix: replace all template addresses with client address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for Saluja Est
 
 Firm facts:
 - Firm: Saluja Estate
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: Commercial Hub, Mohali City Square, Punjab
 - Phone: +91-00000-00000
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
